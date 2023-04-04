04016b970e8b69b3fa0d54f21d778ad2c108e850
MeleryKin <viktoriaack@mail.ru>
2023-04-04 18:50:30 +0300

    Fix controller.

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/controller/LinksController.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/controller/LinksController.java
@@ -1,40 +1,60 @@
 package ru.tinkoff.edu.java.scrapper.controller;
 
-import org.springframework.http.MediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.tinkoff.edu.java.scrapper.dto.request.AddLinkRequest;
 import ru.tinkoff.edu.java.scrapper.dto.request.RemoveLinkRequest;
+import ru.tinkoff.edu.java.scrapper.dto.response.ApiErrorResponse;
 import ru.tinkoff.edu.java.scrapper.dto.response.LinkResponse;
 import ru.tinkoff.edu.java.scrapper.dto.response.ListLinksResponse;
-
-import java.net.URI;
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/links")
 public class LinksController {
-    @GetMapping(
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public ListLinksResponse getLinks(@RequestHeader Long id) {
-        return new ListLinksResponse(new ArrayList<>(), 0);
+    @Operation(summary = "Получить все отслеживаемые ссылки")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ссылки успешно получены", content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = ListLinksResponse.class))
+            })})
+    @GetMapping
+    public ResponseEntity<ListLinksResponse> getAllLinks(@RequestHeader long tgChatId) {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PostMapping(
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public LinkResponse addLink(@RequestHeader Long id,
-                                @RequestBody AddLinkRequest addLinkRequest) {
-        return new LinkResponse(1L, URI.create("temp.url.com/path"));
+    @Operation(summary = "Добавить отслеживание ссылки")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ссылка успешно добавлена", content = {
+                    @Content(mediaType = "appilcation/json", schema =
+                    @Schema(implementation = LinkResponse.class))
+            })
+    })
+    @PostMapping
+    public ResponseEntity<LinkResponse> addLink(@RequestHeader long thChatId,
+                                                @RequestBody AddLinkRequest link) {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @DeleteMapping(
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
-    public LinkResponse deleteLink(@RequestHeader Long id,
-                             @RequestBody RemoveLinkRequest removeLinkRequest) {
-        return new LinkResponse(1L,  URI.create("temp.url.com/path"));
+    @Operation(summary = "Убрать отслеживание ссылки")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ссылка успешно убрана", content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = LinkResponse.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Ссылка не найдена", content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = ApiErrorResponse.class))
+            })
+    })
+    @DeleteMapping
+    public ResponseEntity<LinkResponse> removeLink(@RequestHeader long tgChatId,
+                                                   @RequestBody RemoveLinkRequest link) {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
