df8dd6192e85ba91699458c257852c19dce7fa5b
MeleryKin <viktoriaack@mail.ru>
2023-04-04 11:38:23 +0300

    Fix controller, add version to root pom.

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/controller/LinksController.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/controller/LinksController.java
@@ -1,13 +1,11 @@
 package ru.tinkoff.edu.java.scrapper.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.tinkoff.edu.java.scrapper.dto.request.AddLinkRequest;
 import ru.tinkoff.edu.java.scrapper.dto.request.RemoveLinkRequest;
 import ru.tinkoff.edu.java.scrapper.dto.response.LinkResponse;
 import ru.tinkoff.edu.java.scrapper.dto.response.ListLinksResponse;
-import ru.tinkoff.edu.java.scrapper.service.StackOverflowService;
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -16,29 +14,26 @@ import java.util.ArrayList;
 @RequestMapping("/links")
 public class LinksController {
     @GetMapping(
-            path = "/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public ListLinksResponse getLinks(@PathVariable Long id) {
+    public ListLinksResponse getLinks(@RequestHeader Long id) {
         return new ListLinksResponse(new ArrayList<>(), 0);
     }
 
     @PostMapping(
-            path = "/{id}",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public LinkResponse addLink(@PathVariable Long id,
+    public LinkResponse addLink(@RequestHeader Long id,
                                 @RequestBody AddLinkRequest addLinkRequest) {
         return new LinkResponse(1L, URI.create("temp.url.com/path"));
     }
 
     @DeleteMapping(
-            path = "/{id}",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public LinkResponse deleteLink(@PathVariable Long id,
+    public LinkResponse deleteLink(@RequestHeader Long id,
                              @RequestBody RemoveLinkRequest removeLinkRequest) {
         return new LinkResponse(1L,  URI.create("temp.url.com/path"));
     }
